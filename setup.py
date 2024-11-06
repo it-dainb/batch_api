57ca60ede54eab387f53565c8d946d647323b269
it-dainb <it.dainb@gmail.com>
2024-11-06 10:19:00 +0700

    fix build module

--- setup.py
+++ setup.py
@@ -1,15 +1,10 @@
-import sys
-import platform
 from setuptools import setup, find_packages
 
-# Read the README.md for long description
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
 
-# Define the version
-__version__ = 'v1.0'
+__version__ = '1.0'
 
-# Define the setup configuration
 setup(
     name='OpenaiBatchAPI',
     packages=find_packages(),
@@ -38,16 +33,4 @@ setup(
         'tempfile',
         'tqdm'
     ],
-    # This will help include platform-specific options for wheels
-    options={
-        'bdist_wheel': {
-            'universal': True,  # Build a universal wheel, when possible
-        },
-    },
-    # Optional: specify any extra requirements based on platform
-    extras_require={
-        'dev': ['pytest', 'tox'],
-        'linux': ['libssl-dev'],  # Example for platform-specific dependencies
-        'windows': ['pywin32'],
-    },
 )
